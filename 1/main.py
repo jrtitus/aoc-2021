1489d408e55f09644bb7a1e311a66bb393abdc07
Jeff Titus <jeff.rtitus@gmail.com>
2021-12-11 14:10:07 -0500

    1-b

--- 1/main.py
+++ 1/main.py
@@ -1,9 +1,13 @@
 with open("input.txt") as f:
     lines = f.readlines()
 
+sums = []
+for l in range(2, len(lines)):
+    sums.append(int(lines[l]) + int(lines[l-1]) + int(lines[l-2]))
+
 count = 0
-for l in range(1, len(lines)):
-    if int(lines[l]) > int(lines[l - 1]):
+for s in range(1, len(sums)):
+    if sums[s] > sums[s-1]:
         count += 1
 
 print(count)
