7de10dfeefd3cba737f67e23e97783c63757c464
Jeff Titus <jeff.rtitus@gmail.com>
2021-12-20 13:35:40 -0500

    refactor: 7-b
    use binary search to improve execution time

--- 7/main.py
+++ 7/main.py
@@ -1,3 +1,6 @@
+from math import floor
+
+
 def sumOf(val):
     sum = 0
     for x in range(0, val + 1):
@@ -6,36 +9,43 @@ def sumOf(val):
     return sum
 
 
-with open("input.txt") as f:
-    content = f.read()
-
-# get the list of positions
-positions = content.splitlines()[0].split(",")
-
-# convert strings to numbers and get the max position
-maxPos = 0
-for crab in range(0, len(positions)):
-    positions[crab] = int(positions[crab])
-    if positions[crab] > maxPos:
-        maxPos = positions[crab]
-
-# get fuel calculations for each location
-cost = []
-for pos in range(0, maxPos):
+def getFuelCostForPosition(pos, positions):
     sum = 0
     for crab in positions:
         sum += sumOf(abs(crab - pos))
 
-    cost.append(sum)
+    return sum
 
-# find minimum cost
-costPosition = 0
-minCost = cost[costPosition]
-for idx in range(1, len(cost)):
-    if cost[idx] < minCost:
-        minCost = cost[idx]
-        costPosition = idx
-    elif cost[idx] > minCost:
-        break
 
-print(f"Minimum fuel cost {minCost} found at position {costPosition}")
+def searchForMinCost(positions, minPos, maxPos):
+    currentPos = floor((maxPos + minPos) / 2)
+    cost = getFuelCostForPosition(currentPos, positions)
+    # lowest cost found
+    if maxPos - minPos == 2:
+        return cost, currentPos
+    # check upper; go lower
+    if cost < getFuelCostForPosition(currentPos + 1, positions):
+        return searchForMinCost(positions, minPos, currentPos)
+    # otherwise, go higher
+    else:
+        return searchForMinCost(positions, currentPos, maxPos)
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        content = f.read()
+
+    # get the list of positions
+    positions = content.splitlines()[0].split(",")
+
+    # convert strings to numbers and get the max position
+    maxPos = 0
+    for crab in range(0, len(positions)):
+        positions[crab] = int(positions[crab])
+        if positions[crab] > maxPos:
+            maxPos = positions[crab]
+
+    # find the minimum cost and position
+    minCost, costPosition = searchForMinCost(positions, 0, maxPos)
+
+    print(f"Minimum fuel cost {minCost} found at position {costPosition}")
