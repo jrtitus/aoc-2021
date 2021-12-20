93f5d685a226646c9d336078b78f947471490b92
Jeff Titus <jeff.rtitus@gmail.com>
2021-12-20 09:08:21 -0500

    5-b
    fix: swap y and x to draw matrix correctly

--- 5/main.py
+++ 5/main.py
@@ -15,6 +15,10 @@ def getMaxXY(pairs):
     return [maxX, maxY]
 
 
+def getRange(a, b):
+    return a > b and range(a, b - 1, -1) or range(a, b + 1)
+
+
 with open("input.txt") as f:
     allLines = f.readlines()
 
@@ -25,38 +29,44 @@ for line in allLines:
     [x1, y1] = pairs[0].split(",")
     [x2, y2] = pairs[1].split(",")
 
-    if (x1 == x2) or (y1 == y2):
-        filteredPairs.append([int(x1), int(y1), int(x2), int(y2)])
+    filteredPairs.append([int(x1), int(y1), int(x2), int(y2)])
 
 # find the max X and Y values for those pairs
 [maxX, maxY] = getMaxXY(filteredPairs)
 
 # construct the matrix from (0,0) to (maxX, maxY)
 matrix = []
-for x in range(0, maxX + 1):
+for y in range(0, maxY + 1):
     matrix.append([])
-    for y in range(0, maxY + 1):
-        matrix[x].append(0)
+    for x in range(0, maxX + 1):
+        matrix[y].append(0)
 
 # fill in the matrix
 for xyset in filteredPairs:
     [x1, y1, x2, y2] = xyset
     if y1 == y2:
         # horizontal line
-        hl = x1 > x2 and range(x2, x1 + 1) or range(x1, x2 + 1)
+        hl = getRange(x1, x2)
         for x in hl:
-            matrix[x][y1] += 1
-    else:
+            matrix[y1][x] += 1
+    elif x1 == x2:
         # vertical line
-        vl = y1 > y2 and range(y2, y1 + 1) or range(y1, y2 + 1)
+        vl = getRange(y1, y2)
         for y in vl:
-            matrix[x1][y] += 1
+            matrix[y][x1] += 1
+    else:
+        # diagonal line
+        dlx = getRange(x1, x2)
+        dly = getRange(y1, y2)
+        for idx in range(0, len(dlx)):
+            matrix[dly[idx]][dlx[idx]] += 1
 
-# count the numbers >1 in the matrix
+# count the numbers > 1 in the matrix
 sum = 0
-for x in range(0, len(matrix)):
-    for y in range(0, len(matrix[x])):
-        if matrix[x][y] > 1:
+for y in range(0, len(matrix)):
+    # print(matrix[y])  # uncomment to print the matrix
+    for x in range(0, len(matrix[y])):
+        if matrix[y][x] > 1:
             sum += 1
 
 print(f"Total: {sum}")
